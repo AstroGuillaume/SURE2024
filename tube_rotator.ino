e71be5dd347b2ce51bf10e3ad256d2ebf64c836d
Guillaume <118313328+AstroGuillaume@users.noreply.github.com>
2024-07-04 09:44:50 -0400

    Add files via upload

---
+++ tube_rotator.ino
@@ -0,0 +1,23 @@
+#include <AccelStepper.h>
+
+// Define stepper motor connections and motor interface type
+#define dirPin 4
+#define stepPin 3
+#define motorInterfaceType 1
+
+// Create a new instance of the AccelStepper class
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+
+// Variable to control speed
+float speed = 100.0; // Speed in steps per second
+
+void setup() {
+  // Set the maximum speed
+  stepper.setMaxSpeed(1000); // Set a high max speed to avoid limiting the actual speed
+  stepper.setSpeed(speed); // Set the initial speed
+}
+
+void loop() {
+  // Run the stepper motor at constant speed
+  stepper.runSpeed();
+}
